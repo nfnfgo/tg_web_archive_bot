77223b64e8b95a92a28181c9803591a75b3f53b8
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2022-07-11 12:56:38 +0000

    Update

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ bot.register_message_handler(send_help.send_help, commands=['start', 'help'], pa
 bot.register_callback_query_handler(send_help.get_update, func=lambda query: query.data == 'check_update', pass_bot=True)
 
 # archive url funciton
-bot.register_message_handler(archive.archive_url, commands=['archive','save','a','s'],pass_bot=True)
+bot.register_message_handler(archive.archive_url, commands=['archive', 'save', 'a', 's'], pass_bot=True)
 
 # catch all message
 bot.register_message_handler(archive.archive_url_from_text, content_types=['text'], pass_bot=True)
 # 二次确认 cbq
-bot.register_callback_query_handler(archive.cbq_confirm_archive, func=lambda query: query.data in ['archive_continue_archive','archive_stop_archive'], pass_bot=True)
+bot.register_callback_query_handler(archive.cbq_confirm_archive, func=lambda query: query.data in ['archive_continue_archive', 'archive_stop_archive'], pass_bot=True)
 
 asyncio.run(bot.infinity_polling())
