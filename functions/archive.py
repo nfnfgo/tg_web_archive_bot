8b43aa0c25fc9ed5b4030bad2755f16183ca88dd
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2022-07-12 04:56:04 +0000

    Update

--- functions/archive.py
+++ functions/archive.py
@@ -30,13 +30,13 @@ async def archive_url(message, bot):
         await bot.reply_to(message, '❌*信息读取错误*\n请检查您是否已经在指令后加入正确的网址')
         return
     url = check_url(url)
-    archiving_message = await bot.reply_to(message, f'📸*正在存档...* (新页面时间较长，请耐心等待)\n{url}')
+    archiving_message = await bot.reply_to(message, f'📸<strong>正在存档...</strong> (新页面时间较长，请耐心等待)\n{url}', parse_mode='HTML')
 
     # start to archive webpage by using aiohttp
     async with aiohttp.ClientSession() as session:
         async with session.get('https://web.archive.org/save/'+url) as res:
             # delete archiving msg
-            await bot.delete_message(archiving_message.chat.id,archiving_message.message_id)
+            await bot.delete_message(archiving_message.chat.id, archiving_message.message_id)
             if res.status == 200:
                 await bot.send_message(message.from_user.id, f'*✅成功归档*\n[归档页面地址](https://web.archive.org/save/{url})')
             elif res.status == 520:
@@ -45,8 +45,11 @@ async def archive_url(message, bot):
                 await bot.send_message(message.from_user.id, '❌*归档未完成*\n网址解析失败，可能为填写的网址不规范或机器人内部发生错误')
             else:
                 await bot.send_message(message.from_user.id, '❌*归档未完成*\n请求出错')
+                print(res)
 
 # 用户直接发送消息时
+
+
 async def archive_url_from_text(message, bot):
     print('into the from text function')
     user_status = UserStatus(message=message)
@@ -63,7 +66,7 @@ async def archive_url_from_text(message, bot):
         await bot.reply_to(message, '❌*信息读取错误*\n请检查您是否已经在指令后加入正确的网址')
         return
     url = check_url(url)
-    confirm_msg = await bot.reply_to(message, f'⚠️*是否继续归档*\n*网址*{url}\n\nTips: 通过 /s 指令发出网页归档无需二次确认', reply_markup=text_active_confirm)
+    confirm_msg = await bot.reply_to(message, f'⚠️<strong>是否继续归档</strong>\n<strong>网址</strong>:{url}\n\nTips: 通过 /s 指令发出网页归档无需二次确认', reply_markup=text_active_confirm, parse_mode='HTML')
     # Write confrim_msg to status key 'archive_confirm_msg'
     try:
         user_status.set_status_info({'archive_confirm_msg': confirm_msg})
@@ -89,7 +92,7 @@ async def cbq_confirm_archive(call, bot):
         user_status = UserStatus(call=call)
         archive_confirm_msg = user_status.status_info['archive_confirm_msg'][0]
         await bot.delete_message(archive_confirm_msg.chat.id, archive_confirm_msg.message_id)
-        user_status.set_status_info({'archive_confirm_msg':''})
+        user_status.set_status_info({'archive_confirm_msg': ''})
 
         print('okok before url')
         try:
@@ -98,10 +101,10 @@ async def cbq_confirm_archive(call, bot):
             print('Failed to read url', e)
         print('after url read')
         await bot.answer_callback_query(call.id, '')
-        message = await bot.send_message(call.from_user.id, f'📸*正在存档...* (新页面时间较长，请耐心等待)\n{url}')
+        message = await bot.send_message(call.from_user.id, f'📸<strong>正在存档...</strong> (新页面时间较长，请耐心等待)\n{url}', parse_mode='HTML')
         async with aiohttp.ClientSession() as session:
             async with session.get('https://web.archive.org/save/'+url, timeout=60.0) as res:
-                await bot.delete_message(message.chat.id,message.message_id)
+                await bot.delete_message(message.chat.id, message.message_id)
                 if res.status == 200:
                     await bot.send_message(call.from_user.id, f'*✅成功归档*\n[归档页面地址](https://web.archive.org/save/{url})')
                 elif res.status == 520:
@@ -110,6 +113,7 @@ async def cbq_confirm_archive(call, bot):
                     await bot.send_message(call.from_user.id, '❌*归档未完成*\n网址解析失败，可能为填写的网址不规范或机器人内部发生错误')
                 else:
                     await bot.send_message(call.from_user.id, '❌*归档未完成*\n请求出错')
+                    print(res)
     else:
         await bot.answer_callback_query(call.id, '❎已取消')
         await botton.del_botton(call, bot)
