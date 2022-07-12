7f4c3eac43e51f194deae4a504f9a83ec2ea35ff
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2022-07-12 15:28:23 +0000

    Update

--- services/sql.py
+++ services/sql.py
@@ -1,29 +1,45 @@
 # About asyncio https://aiomysql.readthedocs.io/en/latest/pool.html
 
 import asyncio
-from typing import Coroutine
+
 import aiomysql
 
-import config
+from config import config
 
 
 # thread poo
-class Pool():
+class SQLPool():
     # initialize
+    pool:aiomysql.Pool = None
+
     def __init__(self) -> None:
-        self.created = False
+        '''
+        Actually don't do anything.
+
+        Use await SQLPool.create to create a available pool.
+        '''
         print('services/sql.py: Notice, you should use await Pool.create method to create a pool.')
         pass
 
-    # provide await 
+    # provide await
     def __await__(self):
-        return self.__init().__await__()
+        return self.create().__await__()
 
     # create thread pool
-    async def create(self):
+    async def create(self, force=False) -> aiomysql.Pool:
         '''
-        Create a pool. And set the created value to True(with no actual check)
+        (Asynchronous) Create a pool. And set the created value to True(with no actual check)
+
+        Remember to use await to call this function
+
+        Paras:
+        force: (Bool)(=False) If True, pool will be recreated even if it has a exsiting pool.
         '''
+        # if it's a existing pool, and froce=False, than skip creating and retrun existing pool
+        if (self.pool is not None) and (force == False):
+            return self.pool
+        if self.pool is not None:
+            self.delete()
         self.pool = await aiomysql.create_pool(
             host=config.sql_host,
             port=config.sql_port,
@@ -31,4 +47,28 @@ class Pool():
             password=config.sql_password,
             db=config.sql_db
         )
-        self.created=True
+        return self.pool
+
+    # delete pool
+    async def delete(self, wait=False) -> None:
+        '''
+        (Asynchronous) Delete the thread pool
+
+        Paras:
+        wait: If true, start a coroutine and waiting for all connection to close actually
+        '''
+        # Wait for actual close if wait is True
+        if wait:
+            await self.pool.wait_closed()
+            return
+        self.pool.close()
+
+    # distribute a cursor
+    async def get_cursor(self) -> aiomysql.Cursor:
+        '''
+        (Asynchronous) 
+        Get a cursor from exsiting pool.
+
+        Will Automatically create a pool if not existing
+        '''
+        return
